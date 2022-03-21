c3496f1e42d32e3fb089273b4d4dc8e4d8979996
Vasanth Developer <vsnthdev@gmail.com>
2022-03-21 08:48:39 +0530

    ✨ typing indicator works in magicBox component

--- src/components/magicBox/magicBox.tsx
+++ src/components/magicBox/magicBox.tsx
@@ -3,15 +3,22 @@
  *  Created On 21 March 2022
  */
 
-import { ReactElement } from 'react'
+import { ReactElement, useState } from 'react'
 import FailedIcon from './icons/failed.svg'
 import SuccessIcon from './icons/success.svg'
 import TypingIcon from './icons/typing.svg'
 
 export const MagicBox = (): ReactElement => {
+    // the input value entered in magic box
+    const [text, setText] = useState('')
+
+    // changes typing indicator color basing on whether
+    // there's text or not in the magic box
+    const getTypingIndicatorColor = () => text.length == 0 ? 'text-slate-300' : 'text-slate-500'
+
     return <div id="magicBox" className="relative flex w-[70vw] rounded-xl shadow-box max-w-2xl md:w-[80vw] backdrop-blur-sm bg-white/20 md:text-lg">
         {/* the input where the user enters */}
-        <input autoFocus autoCorrect="off" autoComplete="off" autoCapitalize="false" type="text" className="font-medium m-2 w-full pl-6 pr-16 py-4 rounded-lg outline-none text-slate-600 selection:bg-primary/10 placeholder:text-slate-400 placeholder:overflow-visible" placeholder="@mention, #hashtag or URL" />
+        <input autoFocus autoCorrect="off" autoComplete="off" autoCapitalize="false" type="text" className="font-medium m-2 w-full pl-6 pr-16 py-4 rounded-lg outline-none text-slate-600 selection:bg-primary/10 placeholder:text-slate-400 placeholder:overflow-visible" placeholder="@mention, #hashtag or URL" value={text} onChange={e => setText(e.target.value)} />
 
         <div className="absolute pointer-events-none right-0 h-full px-8 flex items-center">
             {/* failed icon */}
@@ -21,7 +28,7 @@ export const MagicBox = (): ReactElement => {
             <div className="w-5 right-0 top-0 aspect-square text-primary opacity-0"><SuccessIcon/></div>
 
             {/* typing indicator */}
-            <div className="w-5 right-0 top-0 aspect-square text-slate-300"><TypingIcon/></div>
+            <div className={`w-5 right-0 top-0 aspect-square transition-colors duration-500 ${getTypingIndicatorColor()}`}><TypingIcon/></div>
         </div>
     </div>
 }
