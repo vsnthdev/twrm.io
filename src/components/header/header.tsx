30bb77d2d1db6efa8a1f3fa6dd4fba37afda007f
Vasanth Developer <vsnthdev@gmail.com>
2022-03-27 01:05:42 +0530

    🐛 fixed blue gradient leak upon opening settings

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -13,34 +13,39 @@ import SettingsIcon from './icons/settings.svg'
 import { SettingsState, openSettings } from '../settings/functions'
 
 export const Header = ({ settingsState }: { settingsState: SettingsState }): ReactElement => {
-    return <header className="absolute z-50 w-full px-6 py-10 bg-gradient-to-b from-primary to-[transparent]">
-        <div className="px-4 container justify-between flex text-white">
-            <div>
-                <a href="/">
-                    <Logo/>
-                </a>
+    return <>
+        {/* header gradient for PWAs to merge title bar appearance */}
+        <div className="absolute pointer-events-none z-30 w-full h-28 bg-gradient-to-b from-primary to-[transparent]"></div>
+
+        <header className="absolute z-50 w-full px-6 py-10">
+            <div className="px-4 container justify-between flex text-white">
+                <div>
+                    <a href="/">
+                        <Logo/>
+                    </a>
+                </div>
+                <div className="flex items-center text-lg">
+                    {/* GitHub Icon */}
+                    <a href="https://github.com/vsnthdev/twrm.io" target="_blank" rel="noopener" className="mx-2 transition-opacity opacity-70 hover:opacity-100">
+                        <GitHubIcon/>
+                    </a>
+
+                    {/* Twitter Icon */}
+                    <a href="https://vas.cx/twitter" target="_blank" rel="noopener" className="mx-2 transition-opacity opacity-70 hover:opacity-100">
+                        <TwitterIcon/>
+                    </a>
+
+                    {/* Discord Icon */}
+                    <a href="https://vas.cx/discord" target="_blank" rel="noopener" className="mx-2 transition-opacity opacity-70 hover:opacity-100">
+                        <DiscordIcon/>
+                    </a>
+
+                    {/* Settings Cog */}
+                    <button className="ml-4 cursor-pointer" onClick={() => openSettings(settingsState)}>
+                        <SettingsIcon/>
+                    </button>
+                </div>
             </div>
-            <div className="flex items-center text-lg">
-                {/* GitHub Icon */}
-                <a href="https://github.com/vsnthdev/twrm.io" target="_blank" rel="noopener" className="mx-2 transition-opacity opacity-70 hover:opacity-100">
-                    <GitHubIcon/>
-                </a>
-
-                {/* Twitter Icon */}
-                <a href="https://vas.cx/twitter" target="_blank" rel="noopener" className="mx-2 transition-opacity opacity-70 hover:opacity-100">
-                    <TwitterIcon/>
-                </a>
-
-                {/* Discord Icon */}
-                <a href="https://vas.cx/discord" target="_blank" rel="noopener" className="mx-2 transition-opacity opacity-70 hover:opacity-100">
-                    <DiscordIcon/>
-                </a>
-
-                {/* Settings Cog */}
-                <button className="ml-4 cursor-pointer" onClick={() => openSettings(settingsState)}>
-                    <SettingsIcon/>
-                </button>
-            </div>
-        </div>
-    </header>
+        </header>
+    </>
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,7 +9,7 @@ export default () => <Html>
     <Head>
         <meta name="theme-color" content="#0d99fb" />
         <link rel='manifest' href='/manifest.json' />
-        <link rel="icon" type="image/svg" href="/siteIcon.svg" />
+        <link rel="icon" type="image/svg" href="/appIcon.svg" />
     </Head>
     <body className="flex flex-col font-sans bg-slate-100 text-slate-800 selection:bg-primary/10">
         {/* specific page content */}
